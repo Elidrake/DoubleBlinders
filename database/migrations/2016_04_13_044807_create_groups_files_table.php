0935334a63e81b767323c8ae679c8fe21105658f
johnoney <johnoney@email.arizona.edu>
2016-04-20 16:59:54 -0700

    groupsFilesTable

---
+++ database/migrations/2016_04_13_044807_create_groups_files_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateGroupsFilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('groups_files', function (Blueprint $table) {
+            $table->integer('group_id')->unsigned();
+	    $table->integer('file_id')->unsigned();
+	    $table->dateTime('created_at');
+	    $table->dateTime('updated_at');
+	    $table->primary(['group_id','file_id']);
+            $table->unique(['group_id','file_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('groups_files', function (Blueprint $table) {
+            //
+        });
+    }
+}
