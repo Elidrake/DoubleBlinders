67e9d67f246ae545e444e5ee211824dfcd761308
Dakota T <dakota@elidrake.me>
2016-04-25 19:27:16 +0000

    Test

--- app/Group.php
+++ app/Group.php
@@ -37,11 +37,21 @@ class Group extends Model
 			->select('groups.*')
 			->get();
 		if(!empty($your_groups)){
-			foreach($your_groups as $your_group){
-				$your_group->users = DB::table('users_groups')
-					->where('users_groups.group_id', $your_group->id)
-					->select('users_groups.user_id as id', 'users_groups.role')
-					->get();
+			if(Auth::user()->role==1){
+				foreach($your_groups as $your_group){
+					$your_group->users = DB::table('users_groups')
+						->leftJoin('users', 'users_groups.user_id', '=', 'users.id')
+						->where('users_groups.group_id', $your_group->id)
+						->select('users_groups.user_id as id', 'users_groups.role', 'users.name')
+						->get();
+				}
+			}else{
+				foreach($your_groups as $your_group){
+					$your_group->users = DB::table('users_groups')
+						->where('users_groups.group_id', $your_group->id)
+						->select('users_groups.user_id as id', 'users_groups.role')
+						->get();
+				}
 			}
 		}
 
