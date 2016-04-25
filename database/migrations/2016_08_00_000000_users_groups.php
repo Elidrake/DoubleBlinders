006cee9c9de3ea18036d6e46f4ffde77530f2fc7
Dakota T <dakota@elidrake.me>
2016-04-25 16:33:01 +0000

    Testing

---
+++ database/migrations/2016_08_00_000000_users_groups.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UsersGroups extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users_groups', function(Blueprint $table)
+        {
+            $table->interements('id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('assignments');
+        Schema::drop('groups_assignments');
+    }
+}
